e5242ae88eed26c74021677e9a62fae2023a0499
FatmaAbdelmotagly <fatmaabdelmotagly195@gmail.com>
2026-03-06 20:01:41 +0200

    aaa

--- MOBILE-APP/app-backend/src/app.js
+++ MOBILE-APP/app-backend/src/app.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-Account-Key.json");
+const serviceAccount = require("C:/Users/vista store/OneDrive/Desktop/jobPortal/Student-Jobs-Portal/backend/src/config/service-account-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
