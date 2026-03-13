6bb342e29b21ed757d7d3b19325b1731d09a6e30
Sara Mahmoud <saramahmooud3@gmail.com>
2026-03-13 12:58:47 +0200

    add mobile app only

--- backend/src/profiles/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middleware/verifyToken");
-const dashboardController = require("../controllers/dashboardController"); 
-
-router.get("/users", verifyToken, dashboardController.getAllUsers);
-
-module.exports = router;
