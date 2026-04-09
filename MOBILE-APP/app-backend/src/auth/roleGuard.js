67173a3cbce9b38ff99fda7125e07c3cbe8dd79c
FatmaAbdelmotagly <fatmaabdelmotagly195@gmail.com>
2026-04-09 21:57:42 +0200

    update auth

---
+++ MOBILE-APP/app-backend/src/auth/roleGuard.js
@@ -0,0 +1,35 @@
+
+const { doc, getDoc } = require("firebase/firestore");
+const { db } = require("../firebase/firebaseConfig");
+
+const getUserRole = async (uid) => {
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists()) throw new Error("User not found");
+
+  return docSnap.data().role;
+};
+
+const requireStudent = async (uid) => {
+  const role = await getUserRole(uid);
+  if (role !== "student") throw new Error("Access denied: student only");
+};
+
+const requireEmployer = async (uid) => {
+  const role = await getUserRole(uid);
+  if (role !== "employer") throw new Error("Access denied: employer only");
+};
+
+
+const requireAdmin = async (uid) => {
+  const role = await getUserRole(uid);
+  if (role !== "admin") throw new Error("Access denied: admin only");
+};
+
+module.exports = {
+  getUserRole,
+  requireStudent,
+  requireEmployer,
+  requireAdmin
+};
