94d01ce1992d586b8bcdf872978b781ec0ba8e40
FatmaAbdelmotagly <fatmaabdelmotagly195@gmail.com>
2026-04-09 17:34:36 +0200

    comm

--- Backend/src/interactions/interactionRoutes.js
+++ Backend/src/interactions/interactionRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { likeJob, addComment, getComments } = require("./interactionController");
-// ✅ صح
+
 const verifyToken = require("../middleware/verifyToken");
 
 router.post("/like", verifyToken, likeJob);
