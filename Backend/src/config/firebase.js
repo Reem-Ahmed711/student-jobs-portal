1761167d41aa1e71860e6ff5d64da0fb258ef03e
Reem Ahmed <areem4780@gmail.com>
2026-05-09 12:52:57 +0300

    Refactor Firebase initialization logic
    Check if Firebase app is already initialized before creating a new one.

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -1,12 +1,16 @@
+// C:\Student-job-portal\Backend\src\config\firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-account-Key.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// التحقق إذا كان هناك app موجود بالفعل
+if (!admin.apps.length) {
+  const serviceAccount = require("./service-account-key.json");
+  
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const db = admin.firestore();
-
 console.log("🔥 Firebase connected successfully");
 
 module.exports = { admin, db };
