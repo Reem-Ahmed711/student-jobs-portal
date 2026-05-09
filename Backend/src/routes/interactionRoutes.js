74811d3c62a9a225f69300d5fbfceaa1acdbc27b
Reem Ahmed <areem4780@gmail.com>
2026-05-09 12:56:42 +0300

    Remove unnecessary newline in interactionRoutes.js

--- Backend/src/routes/interactionRoutes.js
+++ Backend/src/routes/interactionRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { likeJob, addComment, getComments } = require("../Controllers/interactionController");
-
 const verifyToken = require("../middleware/verifyToken");
 
 router.post("/like", verifyToken, likeJob);
